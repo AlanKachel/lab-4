4f8113e4aa657a49a3732a63d94dcf3c196203e2
AlanKachel <alankachel05@gmail.com>
2024-05-02 10:50:23 +0200

    Wprowadzenie zmian generujących logi w głównym pliku

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Sprawdź, czy podano argument "--date"
 if [ "$1" = "--date" ]; then
-    # Jeśli tak, wyświetl bieżącą datę
     date
+elif [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++)); do
+        # Tworzenie nazwy pliku
+        filename="log${i}.txt"
+        # Wypełnienie pliku danymi
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data utworzenia: $(date)" >> "$filename"
+    done
 else
-    # Jeśli nie, wyświetl komunikat o poprawnym użyciu
     echo "Użycie: $0 --date"
+    echo "Użycie: $0 --logs"
 fi
