ab1ca02f3935b4cf7d1b03608a0ccf885a091f77
AlanKachel <alankachel05@gmail.com>
2024-05-02 11:15:13 +0200

    dodanie flagi --init

--- skrypt.sh
+++ skrypt.sh
@@ -20,14 +20,20 @@ elif [[ "$1" = "--logs" || "$1" = "-l" ]]; then
         echo "Nazwa skryptu: $0" >> "$filename"
         echo "Data utworzenia: $(date)" >> "$filename"
     done
+elif [[ "$1" = "--init" ]]; then
+    git clone https://github.com/AlanKachel/lab-4.git
+
+    export PATH="$PATH:$(pwd)"
 elif [[ "$1" = "--help" || "$1" = "-h" ]]; then
     echo "Użycie: $0 --date (-d)"
     echo "       $0 --logs <liczba_plików> (-l)"
+    echo "       $0 --init"
     echo "       $0 --help (-h)"
     echo ""
     echo "Opis:"
     echo "  -d, --date        Wyświetla bieżącą datę."
     echo "  -l, --logs        Tworzy określoną liczbę plików logów."
+    echo "      --init        Klonuje repozytorium do bieżącego katalogu i dodaje go do PATH."
     echo "  -h, --help        Wyświetla pomoc."
 else
     echo "Nieznana opcja: $1"
