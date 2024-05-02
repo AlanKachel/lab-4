c15db0261face678a906a52d33e4495f83b6547b
AlanKachel <alankachel05@gmail.com>
2024-05-02 10:56:16 +0200

    Dodanie opcji wyboru liczby logów

--- skrypt.sh
+++ skrypt.sh
@@ -3,15 +3,24 @@
 if [ "$1" = "--date" ]; then
     date
 elif [ "$1" = "--logs" ]; then
-    for ((i=1; i<=100; i++)); do
-        # Tworzenie nazwy pliku
+    if [ -z "$2" ]; then
+        echo "Podaj liczbę plików jako drugi argument."
+        exit 1
+    fi
+    
+    re='^[0-9]+$'
+    if ! [[ $2 =~ $re ]] ; then
+        echo "Drugi argument musi być liczbą całkowitą."
+        exit 1
+    fi
+
+    for ((i=1; i<=$2; i++)); do
         filename="log${i}.txt"
-        # Wypełnienie pliku danymi
         echo "Nazwa pliku: $filename" > "$filename"
         echo "Nazwa skryptu: $0" >> "$filename"
         echo "Data utworzenia: $(date)" >> "$filename"
     done
 else
     echo "Użycie: $0 --date"
-    echo "Użycie: $0 --logs"
+    echo "Użycie: $0 --logs <liczba_plików>"
 fi
